3b404111ee504b2dc52a3bc5b8f3e3e4cd1c403c
santt31 <santt31@gmail.com>
2022-04-19 01:33:17 +0200

    finish

--- app/main.js
+++ app/main.js
@@ -225,11 +225,3 @@ const results = () =>{
 document.getElementById("resetGame").addEventListener("click", () => {
   location.reload();
 });
-
-/*video */
-$(document).ready(function() {
-  $('#headerVideoLink').magnificPopup({
-    type:'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });         
-});
